36b83a207df7d199bed3ac9021ca5ed73ad229ed
Federico Melo Barrero <f.melo@uniandes.edu.co>
2022-10-10 15:17:27 -0500

    Mejor caso

--- ProblemaP1.py
+++ ProblemaP1.py
@@ -6,7 +6,31 @@ def max_ocur_subsecuencia() -> int:
     X, Y, m = tuple(stdin.readline().split())
     m = int(m)
     
-    # return num_ocur
+    # Mejor caso: X no contiene ningún caracter de los 2 de Y
+    # o X contiene solo 1 de los caracteres de los 2 de Y.
+    # Ambos O(n) 
+
+    # X no contiene ningún caracter de Y
+    if Y[0] not in X and Y[1] not in X: #2O(n) = O(n)
+        return 1
+    # X contiene solo 1 de los caracteres de Y
+    elif Y[0] not in X or Y[1] not in X: #2O(n)
+        if Y[0] in X: 
+            if X.count(Y[0]) == 1: #O(n)
+                return 2
+            else:
+                return 3
+        elif Y[1] in X:
+            if X.count(Y[1]) == 1: #O(n)
+                return 2
+            else:
+                return 3
+
+    # X contiene ambos caracteres de Y. 
+    else:
+        pass
+        # verdadero algoritmo
+        # return num_ocur
 
 
 def main():
